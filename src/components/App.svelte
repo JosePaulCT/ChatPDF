1a97723a041b68a37b26223e3c331b2493a7ef0e
JosePaulCT <josepaulcamposterrones@gmail.com>
2024-02-11 23:15:46 -0500

    Add svelte-file-dropzone and devDependencies Update tailwind.config.mjs Update App.svelte Update index.astro

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,1 +1,27 @@
-<h1> App </h1>
+<script>
+import { Alert } from 'flowbite-svelte';
+import { APP_STATUS, appStatus } from '../store.ts'
+import { StepUpload } from './StepUpload.svelte';
+import { StepLoading } from './StepLoading.svelte';
+import { StepChat } from './StepChat.svelte';
+</script>
+
+{ #if $appStatus === APP_STATUS.INIT}
+<StepUpload />
+{:else if $appStatus === APP_STATUS.LOADING}
+<StepLoading />
+{:else if $appStatus === APP_STATUS.ERROR}
+<Alert>
+    <span class="font-medium">Algo malo ha pasado!</span>
+    Hay un error en la aplicacion
+</Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+<StepChat />
+{:else}
+<Alert>
+    <span class="font-medium">Acción no reconocida</span>
+    ¿Que estas haciendo? :v
+</Alert>
+{/if}
+
+
---
+++ src/components/StepUpload.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { appStatus } from "../store.ts"
+    import Dropzone from "svelte-file-dropzone"
+
+    let files = {
+    accepted: [],
+    rejected: [],
+    };
+
+    function handleFilesSelect(e) {
+        const { acceptedFiles, fileRejections } = e.detail;
+        files.accepted = [...files.accepted, ...acceptedFiles]
+        files.rejected = [...files.rejected, ...fileRejections]
+    }
+</script>
+<Dropzone accept="application/pdf"  multiple={false}  on:drop={handleFilesSelect} > Arrastra y suelta tu PDF </Dropzone>
+<ol>
+        {#each files.accepted as item}
+        <li>{item.name}</li>
+    {/each}
+    </ol>
+
